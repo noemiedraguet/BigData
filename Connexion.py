20e43fd655222b09040fb65e7ab3e73bed35a64c
Noémie <noemie.draguet@gmail.com>
2024-04-05 21:02:24 +0200

    Query 1 et 2

--- Connexion.py
+++ Connexion.py
@@ -3,22 +3,22 @@ from mysql.connector import errorcode
 import pymongo
 import redis
 
-def mysql_select():
+def mysql_query1():
     try:
         cnx = mysql.connector.connect(user='root', password='password', host='idasm101.unamurcs.be', port='33062', database='reldata')
         cursor = cnx.cursor()
 
-        query = ("SELECT CategoryName, Description FROM Categories WHERE CategoryName in (%s, %s)")
+        query = ("SELECT CategoryName, Description, Picture FROM Categories WHERE Description NOT LIKE (%s)")
 
-        first_parameter = 'Beverages'
-        second_parameter = 'Condiments'
+        chain = '%\sweet%'
 
-        cursor.execute(query, (first_parameter, second_parameter))
+        cursor.execute(query, (chain,))
 
-        for (CategoryName, Description) in cursor:
-            print(f'Category "{CategoryName}" with description "{Description}"')
+        for (CategoryName, Description, Picture) in cursor:
+            print(f'Category: {CategoryName}\nDescription: {Description}\n')
 
         cursor.close()
+
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
@@ -30,15 +30,31 @@ def mysql_select():
         cnx.close()
 
 
-def mongodb_select():
+def mongodb_query2():
     client = pymongo.MongoClient('idasm101.unamurcs.be', 27012)
     db = client['myMongoDB']
     collection = db["Orders"]
-    pipeline = [{"$sample": {"size": 5}}]
-    result = collection.aggregate(pipeline)
+    pipeline = {'OrderID': 10248}
+    result = collection.find(pipeline)
     for doc in result :
-        order_id = doc['OrderID']
-        print(f'Order ID: {order_id}')
+        cust_ID = doc['customer']['CustomerID']
+
+    collection_2 = db["Customers"]
+    second_pipeline = {'ID': cust_ID}
+    customers = collection_2.find(second_pipeline)
+    for cust in customers:
+       id = cust['ID']
+       Name = cust['ContactName']
+       Address = cust['Address']
+       City = cust['City']
+       Postal_Code = cust['PostalCode']
+       Region = cust['Region']
+       Country = cust['Country']
+       Company_Name = cust['CompanyName']
+       Contact_Title = cust['ContactTitle']
+       Fax = cust['Fax']
+       Phone = cust['Phone']
+       print(f'\nID: {id}\nName: {Name}\nAddress: {Address}\nCity: {City}\nPostcode: {Postal_Code}\nRegion: {Region}\nCountry: {Country}\nCompany Name: {Company_Name}\nContact title: {Contact_Title}\nFax: {Fax}\nPhone number: {Phone}\n')
 
         
 def redis_select():
@@ -46,6 +62,6 @@ def redis_select():
     hash_result = r.hget('PRODUCT:72:STOCKINFO', 'UnitsInStock')
     print(f'Product in stock: {hash_result}')
         
-mysql_select()
-mongodb_select()
-redis_select()
+#mysql_query1()
+mongodb_query2()
+#redis_select()
